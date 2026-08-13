d9c0be117938170a0bf1a646b1b75779899eb37e
Yunqi Cai <z5500822@ad.unsw.edu.au>
2026-08-13 21:04:44 +1000

    tab component

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -79,6 +79,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+//Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  //guard clause
+  if (!clicked) return;
+
+  //remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  //activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`);
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 /*
 const h1 = document.querySelector('h1');
 
