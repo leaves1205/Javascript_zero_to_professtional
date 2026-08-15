e7466ec807718d30a93e2a9c29aaf2637921983f
Yunqi Cai <z5500822@ad.unsw.edu.au>
2026-08-16 04:02:09 +1000

    code challeng

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -157,7 +157,7 @@ wait(2)
   .then(() => console.log('I waited for 1 second'));
 Promise.resolve('abc').then(x => console.log(x));
 Promise.reject(new Error('Problem!')).catch(x => console.error(x));
-*/
+
 navigator.geolocation.getCurrentPosition(
   position => {
     const { latitude: lat, longitude: lng } = position.coords;
@@ -171,3 +171,42 @@ const getPosition = function () {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
+
+const imageContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imageContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
+*/
